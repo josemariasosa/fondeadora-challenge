7a7ddd158ec05225fb4f6187528d9fdf804e6066
jomsox@amores <jose.skedar@gmail.com>
2021-08-27 16:01:28 -0500

    db init

--- db/db.py
+++ db/db.py
@@ -7,47 +7,52 @@ import configparser
 
 from sqlite3 import Error
 
-config = configparser.ConfigParser().read('config.ini')
+config = configparser.ConfigParser()
+config.read('config.ini')
 
 
-        
-
-exit()
-
-def connect_db(db_file):
-    conn = None
-
-    try:
-        conn = sqlite3.connect(db_file)
-    except Error:
-       raise Error
-
+def connect_db():
+    db_file = config['sqlite3']['db_file']
+    conn = sqlite3.connect(db_file)
     return conn
 
 
+def build_schema(cursor):
+    db_schema = config['sqlite3']['db_schema']
+    with open(db_schema, 'r') as f:
+        cursor.executescript(f.read())
+        
+
+def insert_records(cursor):
+    sample_data = ['vehicle', 'user', 'location']
+    sample_data_dir = config['sqlite3']['sample_data_dir']
+    for dataset in sample_data:
+        with open(f'./db/sample_data/{dataset}.json', 'r') as ds:
+            records = json.load(ds)
+        columns = records[0].keys()
+        column_text = ', '.join(columns)
+        mask = '(' + ', '.join(['?'] * len(columns)) + ')'
+        rows = [
+            tuple([record[column] for column in columns]) 
+            for record in records
+        ]
+        command = f"INSERT INTO {dataset} ({column_text}) VALUES {mask}"
+        cursor.executemany(command, rows)
+
+
 def init_db():
-    db_file = config[sqlite3][db_file]
-    conn = connect_db(db_file)
+    conn = connect_db()
 
     if conn:
         cursor = conn.cursor()
-        with open('./db/schema.sql') as f:
-            cursor.executescript(f.read().decode('utf-8'))
-        
-        sample_dataset = ['vehicle']
-        for dataset in sample_dataset:
-            with open(f'./db/sample_data/{dataset}.json', 'r') as ds:
-                records = json.load(ds)
-                columns = records[0].keys()
-                column_text = ', '.join(columns)
-                mask = '(' + ', '.join(['?'] * len(columns)) + ')'
-                rows = [
-                    tuple([record[column] for column in columns]) 
-                    for record in records
-                ]
-                command = f"INSERT INTO {dataset} ({column_text}) VALUES {mask}"
+
+        build_schema(cursor)
+        insert_records(cursor)
+
+        conn.commit()
 
         cursor.close()
         conn.close()
+        print('Initialized the database.')
 
 
--- main.py
+++ main.py
@@ -1,56 +1,13 @@
-import psycopg2
+#!/usr/bin/env python3
+# coding=utf-8
+
 from db import db
 
-exit()
 
-conn = psycopg2.connect(
-    dbname='fonderent',
-    user='postgres',
-    password='password',
-    host='localhost',
-    port='5432'
-)
+def main():
+    db.init_db()
 
-vehicles = [
-    {
-        'year': 2013,
-        'make': 'volkswagen',
-        'model': 'jetta',
-        'motor': '4 cil 2.0 lts'
-    },
-    {
-        'year': 2014,
-        'make': 'volkswagen',
-        'model': 'jetta',
-        'motor': '4 cil 2.0 lts'
-    },
-    {
-        'year': 2015,
-        'make': 'volkswagen',
-        'model': 'jetta',
-        'motor': '4 cil 2.0 lts'
-    }
-]
 
-with conn.cursor() as cursor:
-    cursor.execute('DROP TABLE IF EXISTS vehicles')
-    cursor.execute("""
-        CREATE TABLE vehicles (
-            id SERIAL PRIMARY KEY,
-            year INTEGER NOT NULL,
-            make TEXT NOT NULL,
-            model TEXT NOT NULL,
-            motor TEXT NOT NULL
-        );
-    """)
-    
-    for vehicle in vehicles:
-        cursor.execute(
-            'INSERT INTO vehicles (year, make, model, motor) values (%s, %s, %s, %s)',
-            (vehicle['year'], vehicle['make'], vehicle['model'], vehicle['motor'])
-        )
-
-    conn.commit()
-
-conn.close()
+if __name__ == '__main__':
+    main()
 
